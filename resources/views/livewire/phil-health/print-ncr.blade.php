83dca9a8495f13a303d726a5c0ba2f5093d1b26b
beaumat <jason.acosta202@gmail.com>
2026-04-16 13:07:44 +0800

    Update print templates (SOAB and NCR)

--- resources/views/livewire/phil-health/print-ncr.blade.php
+++ resources/views/livewire/phil-health/print-ncr.blade.php
@@ -2,122 +2,115 @@
      style="
         width: 210mm;
         min-height: 297mm;
-        padding: 14mm 16mm 12mm 16mm;
-        font-family: Arial, Helvetica, sans-serif !important;
-        font-size: 13px;
-        line-height: 1.4;
+       padding: 30mm 0 18mm 0;
+        font-family: Arial;
+        text-align:justify;
+        line-height: 1.35;
+        box-sizing: border-box;
      ">
 
-    <!-- Header with logo and Annex -->
-    {{-- <div style="display:flex; font-size:13px;justify-content:space-between; text-align:right; margin-bottom:10px; font-family: Arial, Helvetica, sans-serif;">
-        <div>
-            <img src="{{ asset('dist/logo/philhealth_logo.png') }}"
-                 alt="PhilHealth Logo"
-                 style="width:150px; height:auto;">
-        </div>
-        <div style="font-weight:bold; font-size:13px;">
-            ANNEX A
-        </div>
-    </div> --}}
-
-    <div style="text-align:right; margin-bottom:15px; font-family: Arial, Helvetica, sans-serif;">
-        <div style="font-weight:bold; font-size:18px;">
-            ANNEX A
-        </div>
+    <!-- Annex  padding: 18mm 20mm 18mm 20mm;-->
+    <div style="text-align:right; margin-bottom:24px;">
+        <div style="font-size:20px;">ANNEX A</div>
     </div>
 
     <!-- Title -->
-    <div style="text-align:center; margin-bottom:15px; font-family: Arial, Helvetica, sans-serif;">
-        <div style="font-weight:bold; font-size:25px;">
-            PHILHEALTH NCR CONSENT FORM
-        </div>
+   <div style="text-align:center; margin-bottom:24px; font-family: Arial, Helvetica, sans-serif;">
+    <div style="font-weight:bold; font-size:25px;">
+        PHILHEALTH NCR CONSENT FORM
     </div>
+</div>
 
     <!-- Intro -->
-    <div style="margin-bottom:12px; text-align:justify; font-family: Arial, Helvetica, sans-serif;">
-        <div style="font-weight:bold; font-size:20px; margin-bottom:8px;">
-            PAHINTULOT PARA SA PAGPAPADALA NG BENEFIT PAYMENT NOTICE (BPN) SA PAMAMAGITAN NG SMS AT/O EMAIL
-            
+    <div style="margin-bottom:18px; text-align:justify;">
+      <div style="font-weight:bold; font-size:20px; font-family: Arial; margin-bottom:10px;">
+    PAHINTULOT PARA SA PAGPAPADALA NG BENEFIT PAYMENT NOTICE (BPN) SA <br>
+    PAMAMAGITAN NG SMS AT/O EMAIL
+</div>
+
+        {{-- <div style="margin-bottom:10px; font-size:18px; font-family: Arial, Helvetica, sans-serif">
+            Sa pamamagitan nito, malaya at boluntaryo kong ibinibigay ang aking pahintulot sa <b>PhilHealth Regional Office NCR</b> na padalhan ako ng impormasyon tungkol sa aking Benefit Payment Notice (BPN) o ang detalye ng bayad sa aking confinement/availment gamit ang sumusunod na pamamaraan:
         </div>
 
-        <div style="margin-bottom:6px;font-size:19px;">
-            Sa pamamagitan nito, malaya at boluntaryo kong ibinibigay ang aking pahintulot sa PhilHealth
-            Regional Office NCR na padalhan ako ng impormasyon tungkol sa aking Benefit Payment
-            Notice (BPN) o ang detalye ng bayad sa aking confinement/availment gamit ang sumusunod na
-            pamamaraan:
-        </div>
-        <br>
-
-        <div style="font-style:italic; font-size:19px;">
-            (I hereby freely and voluntarily authorize PhilHealth Regional Office NCR to send information
-            regarding my Benefit Payment Notice (BPN) or the details of my benefit payment for my
-            confinement/availment through the following methods:)
-        </div>
+        <div style="font-style:italic; font-size:12px;">
+            (I hereby freely and voluntarily authorize PhilHealth Regional Office NCR to send information regarding my Benefit Payment Notice (BPN) or the details of my benefit payment for my confinement/availment through the following methods:)
+        </div> --}}
     </div>
 
+      <div style="margin-bottom:10px; font-size:19px; font-family: Arial; text-align:justify;">
+            Sa pamamagitan nito, malaya at boluntaryo kong ibinibigay ang aking pahintulot sa <b>PhilHealth Regional Office NCR</b> na padalhan ako ng impormasyon tungkol sa aking Benefit Payment Notice (BPN) o ang detalye ng bayad sa aking confinement/availment gamit ang sumusunod na pamamaraan:
+        </div>
+
+       <div style="font-style: italic; font-size: 18px; text-align: justify; text-justify: inter-word; line-height: 1.4; width: 100%; display: block;">
+    (I hereby freely and voluntarily authorize PhilHealth Regional Office NCR to send information regarding my Benefit Payment Notice (BPN) or the details of my benefit payment for my confinement/availment through the following methods:)
+</div>
+
+        <br>
+
     <!-- Section I -->
-    <div style="margin-bottom:14px; font-family: Arial, Helvetica, sans-serif;">
-        <div style="font-weight:bold; font-size:20px; margin-bottom:8px">
+    <div style="margin-bottom:24px;">
+        <div style="font-weight:bold; font-size:20px; margin-bottom:10px;">
             I. PAGPILI NG PAMAMARAAN (PLEASE MARK YOUR CHOICE):
         </div>
 
-        <div style="padding-left:20px;">
-            <div style="margin-bottom:6px;font-size:15px;font-family: Arial, Helvetica, sans-serif">
+        <div style="padding-left:22px">
+            <div style="margin-bottom:8px;font-size:19px;">
                 [ ] <b>SMS / TEXT MESSAGE</b>
-                <span style="margin-left:6px;;font-size:15px;font-family: Arial, Helvetica, sans-serif">Cellphone Number:</span>
-                <span style="display:inline-block; min-width:230px; border-bottom:1px solid #000;font-size:15px;font-family: Arial, Helvetica, sans-serif">
+                <span>Cellphone Number:</span>
+                <span style="display:inline-block; width:355px; border-bottom:1px solid #000; line-height:1;">
                     {{ $MOBILE_NO ?? '' }}
                 </span>
             </div>
 
-            <div style="margin-bottom:6px;font-size:15px;font-family: Arial, Helvetica, sans-serif">
+            <div style="margin-bottom:8px;font-size:19px;">
                 [ ] <b>EMAIL</b>
-                <span style="margin-left:6px;font-size:15px;font-family: Arial, Helvetica, sans-serif">Email Address:</span>
-                <span style="display:inline-block; min-width:250px; border-bottom:1px solid #000;font-size:15px;font-family: Arial, Helvetica, sans-serif">
-                   {{ $EMAIL }}
+                <span>Email Address:</span>
+                <span style="display:inline-block; width:540px; border-bottom:1px solid #000; line-height:1;">
+                    {{ $EMAIL ?? '' }}
                 </span>
             </div>
 
-            <div style="margin-bottom:6px;font-size:15px;font-family: Arial, Helvetica, sans-serif">
+            <div style="margin-bottom:8px;font-size:19px;">
                 [ ] <b>Others:</b>
-                <span style="display:inline-block; min-width:300px; border-bottom:1px solid #000;font-size:12px">&nbsp;</span>
+                <span style="display:inline-block; width:670px; border-bottom:1px solid #000; line-height:1;">
+                    &nbsp;
+                </span>
             </div>
         </div>
     </div>
-    <br>
-        <br>
 
     <!-- Section II -->
-    <div style="margin-bottom:16px; text-align:justify; font-family: Arial, Helvetica, sans-serif;">
-        <div style="font-weight:bold; font-size:20px; margin-bottom:8px;">
+    <div style="margin-bottom:28px;">
+        <div style="font-weight:bold;font-size:20px; margin-bottom:10px;">
             II. PAGSANG-AYON AT PAGKILALA (AGREEMENT AND ACKNOWLEDGMENT):
         </div>
-        <br>
 
-        <ol style="margin:0; padding-left:24px;; font-family: Arial, Helvetica, sans-serif; font-size:18px">
-            <li style="margin-bottom:5px;">Nauunawaan ko na ang aking BPN ay naglalaman ng impormasyon tungkol sa aking health insurance benefits.</li>
-            <li style="margin-bottom:5px;">Kinikilala ko na ang pagbibigay ng maling impormasyon (cellphone number o email) ay maaaring maging sanhi ng hindi ko pagtanggap ng abiso.</li>
-            <li style="margin-bottom:5px;">Sumasang-ayon ako na ang PhilHealth ay walang pananagutan kung ang impormasyon ay mabasa ng ibang tao dahil sa maling numerong naibigay o kung ang aking mobile device/email account ay hindi ligtas.</li>
-            <li>Ang pahintulot na ito ay mananatiling may bisa hanggang hindi ko binabawi sa pamamagitan ng isang nakasulat na abiso sa PhilHealth NCR.</li>
+        <ol style="margin:0; padding-left:28px; font-size:19px;">
+            <li style="margin-bottom:6px;">
+                Nauunawaan ko na ang aking BPN ay naglalaman ng impormasyon tungkol sa aking health insurance benefits.
+            </li>
+            <li style="margin-bottom:6px;">
+                Kinikilala ko na ang pagbibigay ng maling impormasyon (cellphone number o email) ay maaaring maging sanhi ng hindi ko pagtanggap ng abiso.
+            </li>
+            <li style="margin-bottom:6px;">
+                Sumasang-ayon ako na ang PhilHealth ay walang pananagutan kung ang impormasyon ay mabasa ng ibang tao dahil sa maling numerong naibigay o kung ang aking mobile device/email account ay hindi ligtas.
+            </li>
+            <li>
+                Ang pahintulot na ito ay mananatiling may bisa hanggang hindi ko binabawi sa pamamagitan ng isang nakasulat na abiso sa PhilHealth NCR.
+            </li>
         </ol>
-        <br>
 
-          <div style="font-style:italic; font-size:18px;">
-            (I understand that my BPN contains sensitive information. I acknowledge that providing
-            incorrect contact details may result in non-receipt of the notice. I agree that PhilHealth
-            shall not be held liable for unauthorized access to the information if the provided contact
-            details are incorrect or if my device/account is compromised. This consent remains valid
-            until revoked in writing.)
+        <div style="font-style:italic; font-size:19px; margin-top:18px;">
+            (I understand that my BPN contains sensitive information. I acknowledge that providing incorrect contact details may result in non-receipt of the notice. I agree that PhilHealth shall not be held liable for unauthorized access to the information if the provided contact details are incorrect or if my device/account is compromised. This consent remains valid until revoked in writing.)
         </div>
-
-       
     </div>
-
     <!-- Signature -->
+    <div style="margin-top:40px;">
+          <!-- Signature -->
    <div style="margin-top:24px; font-family: Arial, Helvetica, sans-serif;">
 
     <!-- Signature line (TOP) -->
-            <div style="text-align:left; margin-bottom:14px; font-size:15px;">
+            <div style="text-align:left; margin-bottom:14px; font-size:19px;">
             
             <b>LAGDA NG MIYEMBRO / KINATAWAN</b>
 
@@ -125,7 +118,7 @@
             <span style="display:inline-block; margin-left:6px;">
                 
                 <!-- Line directly above -->
-                <div style="width:330px; border-top:1px solid #000; margin-bottom:2px;"></div>
+                <div style="width:425px; border-top:1px solid #000; margin-bottom:2px;"></div>
 
                 <!-- Italic text -->
                 <span style="font-style:italic;">
@@ -136,29 +129,24 @@
 
  </div>
 
-    <!-- Name -->
-    <div style="font-family: Arial;margin-bottom:10px;font-size:14px">
-        <b>PANGALAN (PRINTED NAME):</b>
-        <span style="display:inline-block; width:65%; border-bottom:1px solid #000;font-family: Arial"><b>{{ $MEMBER_LAST_NAME }}, {{ $MEMBER_FIRST_NAME }} {{ $MEMBER_MIDDLE_NAME }}</b></span>
+        <div style="margin-bottom:12px; font-size:19px;">
+            <b>PANGALAN (PRINTED NAME):</b>
+            <span style="display:inline-block; width:63%; border-bottom:1px solid #000; line-height:1;">
+                {{ $MEMBER_LAST_NAME ?? '' }}, {{ $MEMBER_FIRST_NAME ?? '' }} {{ $MEMBER_MIDDLE_NAME ?? '' }}
+            </span>
+        </div>
+
+        <div style="display:flex; align-items:center; gap:14px; font-size:19px;">
+            <span><b>PhilHealth ID Number (PIN):</b></span>
+            <span style="flex:1; border-bottom:1px solid #000; line-height:1;">
+                {{ $PIN ?? '' }}
+            </span>
+
+            <span><b>Petsa (Date):</b></span>
+            <span style="flex:1; border-bottom:1px solid #000; line-height:1">
+                {{ $DATE_DISCHARGED ?? '' }}
+            </span>
+        </div>
     </div>
 
-    <!-- PIN and Date -->
-    <div style="margin-top:8px">
-
-    <div style="font-family: Arial;margin-bottom:10px;font-size:14px">
-
-        <!-- PIN -->
-        <span style="font-family: Arial;margin-bottom:10px;font-size:14px"><b>PhilHealth ID Number (PIN):</b></span>
-        <span style="flex:1; border-bottom:1px solid #000; height:14px;font-size:14px"><b>{{ $PIN }}</b></span>
-
-        <!-- Date -->
-        <span style="margin-left:20px;font-size:14px"><b>Petsa (Date):</b></span>
-        <span style="flex:1; border-bottom:1px solid #000; height:14px;font-size:14px">{{ $DATE_DISCHARGED }}</span>
-
-    </div>
-
-</div>
-
-</div>
-    </div>
 </div>
